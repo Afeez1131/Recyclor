6bfd914c5ad8898d33c3f31b29d56280c9286ae0
Lawal Afeez <lawalafeez052@gmail.com>
2024-06-12 17:26:36 +0100

    wrote test for RegisterForm

---
+++ account/tests/test_forms.py
@@ -0,0 +1,97 @@
+from django.test import TestCase, Client
+from account.forms import RegisterForm
+from django.contrib.auth.models import User
+from django.forms import Form
+
+
+class RegistrationFormTestCase(TestCase):
+    def setUp(self):
+        self.form = RegisterForm()
+        self.fields = ["full_name", "email", "password1", "password2"]
+        self.data = {
+            "full_name": "Recyclor",
+            "email": "mail@gmail.com",
+            "password1": "testpass123",
+            "password2": "testpass123"
+        }
+
+    def test_registration_form_is_valid(self):
+        self.assertIsInstance(self.form, Form)
+
+    def test_registration_form_fields_exists(self):
+        self.assertIn("full_name", self.form.fields)
+        self.assertIn("email", self.form.fields)
+        self.assertIn("password1", self.form.fields)
+        self.assertIn("password2", self.form.fields)
+
+    def test_registration_form_fields_attrs(self):
+        self.assertEqual(self.form.fields["full_name"].max_length, 100)
+        self.assertEqual(self.form.fields["full_name"].required, True)
+
+        self.assertEqual(self.form.fields["email"].max_length, 100)
+        self.assertEqual(self.form.fields["email"].required, True)
+
+    def test_registration_form_fields_widget_attrs(self):
+        for field in self.fields:
+            field_widget = self.form.fields[field].widget
+            self.assertEqual(field_widget.attrs.get("class"), "form-control")
+        self.assertEqual(
+            self.form.fields["full_name"].widget.attrs.get("placeholder"), "Full Name"
+        )
+        self.assertEqual(
+            self.form.fields["email"].widget.attrs.get("placeholder"), "Email"
+        )
+
+    def test_registration_form_with_invalid_email(self):
+        self.data.update({"email": "email.com"})
+        form = RegisterForm(self.data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("email", form.errors.keys())
+        self.assertIn("Enter a valid email address.", form.errors.get("email"))
+        
+    def test_registration_form_with_existing_mail(self):
+        User.objects.create_user(
+            username="testing", password="123445", email="email@gmail.com"
+        )
+        self.data.update({"email": "email@gmail.com"})
+        form = RegisterForm(self.data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("email", form.errors.keys())
+        self.assertIn("User with this email exists", form.errors.get("email"))
+        
+    def test_registration_form_without_password(self):
+        self.data.pop("password1", None)
+        self.data.pop("password2", None)  
+        form = RegisterForm(self.data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("password1", form.errors.keys())
+        self.assertIn("password2", form.errors.keys())
+        self.assertIn("Password is required", form.errors.get("password1"))
+        self.assertIn("Password is required", form.errors.get("password2"))
+        
+    def test_registration_form_with_password_mismatch(self):
+        self.data.update({"password2": "testpass1234"})
+        form = RegisterForm(self.data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("password1", form.errors.keys())
+        self.assertIn("password2", form.errors.keys())
+        self.assertIn("Passwords do not match", form.errors.get("password1"))        
+        self.assertIn("Passwords do not match", form.errors.get("password2"))
+        
+    def test_registration_form_with_invalid_password(self):
+        self.data.update({"password1": "password", "password2": "password"})
+        form = RegisterForm(self.data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("password1", form.errors.keys())
+        self.assertIn("This password is too common.", form.errors.get('password1'))
+
+    def test_registration_form_save_with_valid_data(self):
+        form = RegisterForm(self.data)
+        if form.is_valid():
+            response = form.save()
+            user = response.get("user")
+            self.assertTrue(form.is_valid())
+            self.assertTrue(form.is_bound)
+            self.assertDictEqual(form.errors, {})
+            self.assertTrue(User.objects.filter(id=user.id).exists())
+            self.assertEqual(user.email, self.data.get("email"))
---
+++ core/tests/test_views.py
@@ -0,0 +1,102 @@
+from django.conf.global_settings import LOGIN_URL
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, Client, RequestFactory
+from django.urls import reverse
+from core.views import DashboardView
+
+def get_user(username="testuser", password="password"):
+    return {"username": username, "password": password}
+
+DASHBOARD_URL = reverse('core:dashboard')
+LOGIN_URL = reverse('account:login')
+
+class DashboardViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_data = get_user()
+        self.user = User.objects.create_user(**self.user_data)
+
+    def test_dashboard_view_without_authentication(self):
+        response = self.client.get(DASHBOARD_URL)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"{LOGIN_URL}?next={DASHBOARD_URL}")
+        
+    def test_dashbboard_view_with_authentication(self):
+        self.client.login(**self.user_data)
+        response = self.client.get(DASHBOARD_URL)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "core/dashboard.html")
+        
+    def test_dashboard_view_valid_context(self):
+        self.client.login(**self.user_data)
+        response = self.client.get(DASHBOARD_URL)
+        context = response.context
+        self.assertIn("title", context)
+        self.assertEqual(context.get('title'), "Dashboard")
+        
+    def test_dashboard_view_invalid_context(self):
+        self.client.login(**self.user_data)
+        response = self.client.get(DASHBOARD_URL)
+        context = response.context
+        self.assertNotIn("invalidcontext", context)
+        self.assertNotIn("form", context)
+        
+"""
+The next section would be using RequestFactory for test, instead of the TestCase.
+Why RequestFactory?
+- It allows testing the view in isolation without the complete request -> response cycle.
+- since it does not require the request -> response cycle, it is faster than using the TestCase.
+- since I am not working with database
+- middleware is sideline, we are only dealing with the view itself.
+
+TestCase?
+- It is slower than RequestFactory
+- It completes the request -> response cycle
+- can access database
+"""
+
+
+class DashboardViewRequestFactoryTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user_data = get_user()
+        self.user = User.objects.create_user(**self.user_data)
+        
+    def test_dashboard_view_without_authentication(self):
+        request = self.factory.get(DASHBOARD_URL)
+        request.user = AnonymousUser()
+        response = DashboardView.as_view()(request)
+        self.assertEqual(response.status_code, 302)
+        redirect_to = f"{LOGIN_URL}?next={DASHBOARD_URL}"
+        self.assertEqual(response.get('location'), redirect_to)
+        
+    def test_dashboard_view_with_authentication(self):
+        request = self.factory.get(DASHBOARD_URL)
+        request.user = self.user
+        response = DashboardView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+    # 
+    def test_dashboard_view_context(self):
+        request = self.factory.get(DASHBOARD_URL)
+        request.user = self.user
+        response = DashboardView.as_view()(request)
+        self.assertIn("title", response.context_data)
+        self.assertEqual(response.context_data.get("title"), "Dashboard")
+        
+    def test_dashboard_view_invalid_context(self):
+        request = self.factory.get(DASHBOARD_URL)
+        request.user = self.user
+        
+        response = DashboardView.as_view()(request)
+        context = response.context_data
+        self.assertNotIn("invalidcontext", context)
+        
+    def test_dashboard_view_template(self):
+        request = self.factory.get(DASHBOARD_URL)
+        request.user = self.user
+        response = DashboardView.as_view()(request)
+        self.assertIn("core/dashboard.html", response.template_name)
+        
+        
+        
+    
